b9a10d4aa4570c427b9622aa9dfeb47778d16995
Liudmila Gonchikova <gonchikova94@gmail.com>
2021-04-16 00:09:06 +0800

    style

--- src/main/resources/static/js/header/toolbar.js
+++ src/main/resources/static/js/header/toolbar.js
@@ -1,12 +1,14 @@
+import {getOtherWidth, main_body_width} from "../general.js";
+
 function getHeaderLabel(size) {
     return {
+        borderless: true,
         rows: [
             {
-                template: '<span style="font-size: larger">Микрокредитная компания</span><br>' +
-                    '<span style="font-size: larger">Фонд поддержки малого предпринимательства Республики Бурятия</span><br>' +
-                    '<span style="font-size: smaller">ОГРН 1020300978147, ИНН 0323072429, КПП 032301001</span><br>' +
-                    '<span style="font-size: smaller">670000, Республика Бурятия, г. Улан-Удэ,ул. Партизанская 28</span>',
-                borderless: true,
+                template: '<span style="font-size: larger; font-family:Montserrat, serif; font-weight: bolder;">Микрокредитная компания</span><br>' +
+                    '<span style="font-size: larger; font-family:Montserrat, serif; font-weight: bolder;">Фонд поддержки малого предпринимательства Республики Бурятия</span><br>' +
+                    '<span style="font-size: smaller; font-family:Montserrat, serif;">ОГРН 1020300978147, ИНН 0323072429, КПП 032301001</span><br>' +
+                    '<span style="font-size: smaller; font-family:Montserrat, serif;">670000, Республика Бурятия, г. Улан-Удэ,ул. Партизанская 28</span>',
             }
         ]
     }
@@ -16,26 +18,37 @@ function getHeader(size) {
     var headerLabel = getHeaderLabel(size);
 
     return {
-        view: 'toolbar',
-        id: 'headerId',
-        height: size,
-        elements: [
+        borderless: true,
+        cols: [
+            { width: getOtherWidth()},
             {
-                view: 'button',
-                type: 'image',
-                image: '../imgs/icon_fond.webp',
-                width: size,
+                view: 'toolbar',
+                id: 'headerId',
+                height: size,
+                width: main_body_width,
                 borderless: true,
+                elements: [
+                    {
+                        view: 'button',
+                        type: 'image',
+                        image: '../imgs/icon_my_business.webp',
+                        width: size,
+                        borderless: true,
+                    },
+                    {
+                        view: 'button',
+                        type: 'image',
+                        image: '../imgs/icon_fond.webp',
+                        width: size,
+                        borderless: true,
+                    },
+                    headerLabel,
+                ]
             },
-            headerLabel,
-            {
-                view: 'button',
-                type: 'image',
-                image: '../imgs/icon_my_business.webp',
-                width: size,
-                borderless: true,
-            },
+            { width: getOtherWidth()},
+
         ]
+
 
     }
 }
