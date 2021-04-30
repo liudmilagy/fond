393246d34545059f99e84ffbf895f97e3a6c93f8
liudmilagy <gonchikova94@gmail.com>
2021-04-30 15:06:03 +0800

    news

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -11,6 +11,7 @@ import {carousel} from "./header/carousel.js";
 import {calculator} from "./calculator/calculator.js";
 import {map} from "./map/map.js"
 import {footer} from "./footer/footer.js";
+import {news} from "./news/news_main.js";
 
 function getClientWidth() {
     return document.body.clientWidth;
@@ -50,6 +51,8 @@ webix.ready(function() {
                                 {},
                                 calculator(),
                                 {},
+                                {},
+                                news(),
                                 {},
                                 map(),
                                 {},
