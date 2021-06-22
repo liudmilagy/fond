125d870a5e6c23e65bfd37bf3fade8d9e1c82d60
liudmila <gonchikova94@gmail.com>
2021-06-22 22:40:24 +0800

    sms

--- src/main/java/ru/gly/fond/service/SmsServiceImpl.java
+++ src/main/java/ru/gly/fond/service/SmsServiceImpl.java
@@ -11,17 +11,42 @@ import java.text.SimpleDateFormat;
 @Service
 public class SmsServiceImpl implements SmsService {
 
-    @Value("${websms.login}")
-    private String user;
+//    @Value("${websms.login}")
+//    private String user;
+//
+//    @Value("${websms.password}")
+//    private String password;
 
-    @Value("${websms.password}")
+    @Value("${smsc.login}")
+    private String login;
+
+    @Value("${smsc.password}")
     private String password;
+
+//    @Override
+//    public void sendMessage(RegClientAppointment clientAppointment) {
+//        HttpSmsSender ws = new HttpSmsSender(user, password);
+//
+//        String clientPhone = clientAppointment.getPhoneNumber();
+//        DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+//        DateFormat tf = new SimpleDateFormat("hh:mm");
+//        if (!clientPhone.isEmpty()) {
+//            ClsTypeAppointment typeAppointment = clientAppointment.getClsTypeAppointment();
+//            String message = clientAppointment.getClientName() + ", Вы успешно записаны на '" + typeAppointment.getName() + "'. \n" +
+//                    "Дата: " + df.format(clientAppointment.getDate()) + "\n" +
+//                    "Время: " + tf.format(clientAppointment.getTime());
+//            int responseCode = ws.send(clientPhone, message, "", Integer.parseInt(""+clientAppointment.getId()), false, false);
+//            System.out.println(responseCode);
+//            System.out.println(ws.getStatus());
+//        }
+//    }
 
     @Override
     public void sendMessage(RegClientAppointment clientAppointment) {
-        HttpSmsSender ws = new HttpSmsSender(user, password);
+        Smsc smsc = new Smsc(login, password);
 
         String clientPhone = clientAppointment.getPhoneNumber();
+        clientPhone.replaceAll("[^0-9]+", "");
         DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
         DateFormat tf = new SimpleDateFormat("hh:mm");
         if (!clientPhone.isEmpty()) {
@@ -29,9 +54,7 @@ public class SmsServiceImpl implements SmsService {
             String message = clientAppointment.getClientName() + ", Вы успешно записаны на '" + typeAppointment.getName() + "'. \n" +
                     "Дата: " + df.format(clientAppointment.getDate()) + "\n" +
                     "Время: " + tf.format(clientAppointment.getTime());
-            int responseCode = ws.send(clientPhone, message, "", Integer.parseInt(""+clientAppointment.getId()), false, false);
-            System.out.println(responseCode);
-            System.out.println(ws.getStatus());
+            String[] ret = smsc.send_sms(clientPhone, message, 0, "", clientAppointment.getId().toString(), 0, "", "" );
         }
     }
 }
