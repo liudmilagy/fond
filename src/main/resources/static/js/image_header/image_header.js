7cc9c50090ee19409f26f202d28fb54de471f4c4
liudmila <gonchikova94@gmail.com>
2021-06-27 09:41:31 +0800

    remove label on header image

--- src/main/resources/static/js/image_header/image_header.js
+++ src/main/resources/static/js/image_header/image_header.js
@@ -3,12 +3,12 @@ export const image_header= {
     height: document.body.clientWidth * 824 / 1924,
     template: '<div class="container">' +
     '<img src="https://testfond.hb.bizmrg.com/fond_bg/bg_uu.webp" style="width:100%; object-fit: contain">' +
-    '<div class="heading">' +
-    'ПОДДЕРЖКА' +
-    '</div>'+
-    '<div class="heading2">'+
-    'субъектов малого и среднего предпринимательства<br>' +
-    'Республики Бурятия\n' +
-    '</div>'+
+    // '<div class="heading">' +
+    // 'ПОДДЕРЖКА' +
+    // '</div>'+
+    // '<div class="heading2">'+
+    // 'субъектов малого и среднего предпринимательства<br>' +
+    // 'Республики Бурятия\n' +
+    // '</div>'+
     '</div>'
 }
