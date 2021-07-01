7c32d98cae59a7b6dcd4165d192e5a17c34f5998
liudmila <gonchikova94@gmail.com>
2021-07-01 17:02:46 +0800

    fix

--- src/main/java/ru/gly/fond/service/EmailServiceImpl.java
+++ src/main/java/ru/gly/fond/service/EmailServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import ru.gly.fond.model.ClsTypeAppointment;
 import ru.gly.fond.model.RegClientAppointment;
 
@@ -14,7 +15,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 @Log4j2
-@Controller
+@Service
 public class EmailServiceImpl extends SuperServiceImpl implements EmailService {
 
     @Autowired
@@ -22,6 +23,10 @@ public class EmailServiceImpl extends SuperServiceImpl implements EmailService {
 
     @Value("${spring.mail.from}")
     private String fromAddress;
+
+    @Value("${fond.mail}")
+    private String fondMail;
+
 
     @Override
     public void sendSimpleMessage(String to, String from, String subject, String text) throws MailException {
@@ -52,6 +57,13 @@ public class EmailServiceImpl extends SuperServiceImpl implements EmailService {
                             "Время: " + tf.format(clientAppointment.getTime());
 
             sendSimpleMessage(clientEmail, fromAddress, subject, message);
+
+
+            String messageToFond = "Клиент: " + clientAppointment.getClientName() + "\n" +
+                    "Тип записи: " + typeAppointment.getName() + "\n" +
+                    "Дата: " + df.format(clientAppointment.getDate()) + "\n" +
+                    "Время: " + tf.format(clientAppointment.getTime());
+            sendSimpleMessage(fondMail, fromAddress, subject, messageToFond);
         }
     }
 }
